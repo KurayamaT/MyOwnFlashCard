5c3184d190adfd635988bea445c8f55572fd7388
KurayamaT <t.kurayama@nifty.com>
2020-08-03 22:05:12 +0900

    大体終わりました。最終確認してください。

--- app/src/main/java/net/minpro/myownflashcard/MainActivity.kt
+++ app/src/main/java/net/minpro/myownflashcard/MainActivity.kt
@@ -1,67 +1,67 @@
 package net.minpro.myownflashcard
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-import android.content.Intent as Intent
+
 
 var intBackGroundColor = 0
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var timer: Timer = Timer()
-
-    //      click process of button
-    //    「単語を編集」ボタンを押した場合
-    //    単語一覧画面(WordListActivity)へ
+        //ボタンのクリック処理
+        //    「単語を編集」ボタンを押した場合
+        //     単語一覧画面(WordListActivity)へ
         buttonEdit.setOnClickListener {
             val intent = Intent(this@MainActivity, WordListActivity::class.java)
             startActivity(intent)
         }
-    //    「色」ボタンを押した場合
-    //    画面の背景色をボタンの色に設定
 
+        //    「色」ボタンを押した場合
+        //     画面の背景色をボタンの色に設定
         buttonColor01.setOnClickListener {
             intBackGroundColor = R.color.color01
-            constraintlayoutMain.setBackgroundResource(R.color.color01)
+            constraintLayoutMain.setBackgroundResource(intBackGroundColor)
         }
         buttonColor02.setOnClickListener {
             intBackGroundColor = R.color.color02
-            constraintlayoutMain.setBackgroundResource(R.color.color02)
+            constraintLayoutMain.setBackgroundResource(intBackGroundColor)
         }
         buttonColor03.setOnClickListener {
             intBackGroundColor = R.color.color03
-            constraintlayoutMain.setBackgroundResource(R.color.color03)
+            constraintLayoutMain.setBackgroundResource(intBackGroundColor)
         }
         buttonColor04.setOnClickListener {
             intBackGroundColor = R.color.color04
-            constraintlayoutMain.setBackgroundResource(R.color.color04)
+            constraintLayoutMain.setBackgroundResource(intBackGroundColor)
         }
         buttonColor05.setOnClickListener {
             intBackGroundColor = R.color.color05
-            constraintlayoutMain.setBackgroundResource(R.color.color05)
+            constraintLayoutMain.setBackgroundResource(intBackGroundColor)
         }
         buttonColor06.setOnClickListener {
             intBackGroundColor = R.color.color06
-            constraintlayoutMain.setBackgroundResource(R.color.color06)
+            constraintLayoutMain.setBackgroundResource(intBackGroundColor)
         }
 
+//        「かくにんテスト」ボタンを押した場合
         buttonTest.setOnClickListener {
+            //         テスト画面(TestActivity)へ
+            //        => 選択したテスト条件をIntentで渡す
             val intent = Intent(this@MainActivity, TestActivity::class.java)
-            when(radioGroup.checkedRadioButtonId){
+            when (radioGroup.checkedRadioButtonId) {
+                //暗記済みの単語を除外する場合
                 R.id.radioButton -> intent.putExtra(getString(R.string.intent_key_memory_flag), true)
+                //暗記済み単語を除外しない場合（＝含める）
                 R.id.radioButton2 -> intent.putExtra(getString(R.string.intent_key_memory_flag), false)
-
             }
             startActivity(intent)
         }
-
-
     }
 }
--- app/src/main/java/net/minpro/myownflashcard/TestActivity.kt
+++ app/src/main/java/net/minpro/myownflashcard/TestActivity.kt
@@ -1,8 +1,9 @@
 package net.minpro.myownflashcard
 
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
 import io.realm.Realm
 import io.realm.RealmResults
 import kotlinx.android.synthetic.main.activity_test.*
@@ -11,130 +12,220 @@ import kotlin.collections.ArrayList
 
 class TestActivity : AppCompatActivity(), View.OnClickListener {
 
+    //テスト条件（暗記済みの単語を除外するか：する＝＞true）
     var boolStatusMemory: Boolean = false
 
-    // test situation
-    var intState: Int = 0
-    val BEFORE_START: Int = 1
-    val RUNNING_QUESTION = 2
-    val RUNNING_ANSWER: Int = 3
-    val TEST_FINISHED: Int = 4
+    //問題を暗記済みにするかどうか
+    var boolMemorized: Boolean = false
 
-    //realm 関連
+    //テストの状態
+    var intState: Int = 0
+    val BEFORE_START: Int = 1       //テスト開始前
+    val RUNNING_QUESTION: Int = 2   //問題を出した段階
+    val RUNNING_ANSWER: Int = 3     //こたえを出した段階
+    val TEST_FINISHED: Int = 4      //テスト終了
+
+    //realm関係
     lateinit var realm: Realm
     lateinit var results: RealmResults<WordDB>
-    lateinit var word_list: ArrayList<WordDB>
+    lateinit var wordList: ArrayList<WordDB>
 
-    var intLength:Int = 0 //number of test records
+    var intLength: Int = 0   //レコードの数(テストの問題数）
+    var intCount: Int = 0    //今何問めを示すカウンター
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
-//Todo 画面が開いたとき
-//Todo 1.MainActivityからのIntent(テスト条件)受け取り
-    val bundle = intent.extras
-    boolStatusMemory = bundle!!.getBoolean(getString(R. string.intent_key_memory_flag))
 
-//Todo 2.前画面で設定した背景色を設定
-    constraintLayoutTest.setBackgroundResource(intBackGroundColor)
+        // 画面が開いたとき
+        // 1.MainActivityからのIntent(テスト条件)受け取り
+        val bundle = intent.extras
+        boolStatusMemory = bundle!!.getBoolean(getString(R.string.intent_key_memory_flag))
 
-//Todo 3.テスト状態を「開始前」に+カード画像非表示
+        // 2.前画面で設定した背景色を設定
+        constraintLayoutTest.setBackgroundResource(intBackGroundColor)
+
+        // 3.テスト状態を「開始前」に+カード画像非表示
         intState = BEFORE_START
         imageViewFlashQuestion.visibility = View.INVISIBLE
-        imageViewFrashAnswer.visibility = View.INVISIBLE
+        imageViewFlashAnswer.visibility = View.INVISIBLE
 
-//Todo 4.ボタン①を「テストをはじめる」に
+        // 4.ボタン①を「テストをはじめる」に
         buttonNext.setBackgroundResource(R.drawable.image_button_test_start)
 
-//Todo 5.ボタン②を「かくにんテストをやめる」に
+        //5.ボタン②を「かくにんテストをやめる」に
         buttonEndTest.setBackgroundResource(R.drawable.image_button_end_test)
 
-//click listener
+        //クリックリスナー
         buttonNext.setOnClickListener(this)
         buttonEndTest.setOnClickListener(this)
-
+        checkBox.setOnClickListener {
+            //暗記済みのチェックボックスを押したときは暗記済みのステータスを変える
+            boolMemorized = checkBox.isChecked
+        }
     }
+
 
     override fun onResume() {
         super.onResume()
 
-        //realm get instance
+        //realmインスタンスの取得
         realm = Realm.getDefaultInstance()
 
-        //5.get data from DB
+        //5.DBからテストデータ取得(テスト条件で処理分岐)
         if (boolStatusMemory) {
-            // reject remarked words
-            results = realm.where(WordDB::class.java)
-                .equalTo(getString(R.string.db_field_memory_flag), false).findAll()
-        }else{
-            // include remarked words
+            //暗記済みの単語を除外する
+            results = realm.where(WordDB::class.java).equalTo(getString(R.string.db_field_memory_flag), false).findAll()
+        } else {
+            //暗記済みの単語を除外しない（含める）
             results = realm.where(WordDB::class.java).findAll()
-
         }
 
-        //
+        //残り問題数の表示
         intLength = results.size
         textViewRemaining.text = intLength.toString()
 
-
-        //6. shuffle data of 5
-        word_list = ArrayList(results)
-        Collections.shuffle(word_list)
+        //6. 5で取得したテストデータをシャッフル
+        wordList = ArrayList(results)
+        Collections.shuffle(wordList)
     }
 
     override fun onPause() {
         super.onPause()
 
-        //realm
+        //realmの後片付け
         realm.close()
-
     }
 
+    override fun onClick(v: View) {
 
-    override fun onClick(v: View?){
+        when (v.id) {
+            // ボタン①（上のボタン）を押したとき
+            //＝＞テスト状態によって処理を変える
+            R.id.buttonNext ->
 
-//Todo ボタン①（上のボタン）を押したとき
-//＝＞テスト状態によって処理を変える
-//Todo 1. 「テスト開始前」の場合
-//=> 「問題を出した段階」に+問題表示(showQuestion)
-        showQuestion()
-// Todo 2.「問題を出した段階」の場合
-//=> 「こたえを出した段階」に+答え表示(showAnswer)
-        showAnswer()
-//Todo 3.「こたえを出した段階」の場合
-//=> 「問題を出した段階」に+問題表示(showQuestion)
-        showQuestion()
+                when (intState) {
+                    BEFORE_START -> {
+                        //1. 「テスト開始前」の場合
+                        //=> 「問題を出した段階」に+問題表示(showQuestion)
+                        intState = RUNNING_QUESTION
+                        showQuestion()
+                    }
 
-//Todo ボタン②（下のボタン）を押したとき：確認ダイアログ
-//Todo 1. テスト画面を閉じてMainActivityに戻る
-//＝＞Todo テスト状態が「テスト終了」の場合
-//=> 最後の問題の暗記済フラグをDBに登録(更新)
+                    RUNNING_QUESTION -> {
+                        //2.「問題を出した段階」の場合
+                        //=> 「こたえを出した段階」に+答え表示(showAnswer)
+                        intState = RUNNING_ANSWER
+                        showAnswer()
+                    }
 
+                    RUNNING_ANSWER -> {
+                        // 3.「こたえを出した段階」の場合
+                        //=> 「問題を出した段階」に+問題表示(showQuestion)
+                        intState = RUNNING_QUESTION
+                        showQuestion()
+                    }
+
+                }
+
+            R.id.buttonEndTest -> {
+                // ボタン②（下のボタン）を押したとき：確認ダイアログ
+                val dialog = AlertDialog.Builder(this@TestActivity).apply {
+                    setTitle("テストの終了")
+                    setMessage("テストを終了してもいいですか？")
+                    setPositiveButton("はい") { dialog, which ->
+                        // 1. テスト画面を閉じてMainActivityに戻る
+                        //＝＞ テスト状態が「テスト終了」の場合
+                        //=> 最後の問題の暗記済フラグをDBに登録(更新)
+                        if (intState == TEST_FINISHED) {
+                            val selectedDB = realm.where(WordDB::class.java).equalTo(getString(R.string.db_field_question)
+                                , wordList[intCount - 1].strQuestion).findFirst()
+                            realm.beginTransaction()
+                            selectedDB?.boolMemoryFrag = boolMemorized
+                            realm.commitTransaction()
+                        }
+                        finish()
+                    }
+                    setNegativeButton("いいえ") { dialog, which -> }
+                    show()
+
+                }
+            }
+        }
     }
-
-
 
     private fun showAnswer() {
-//Todo こたえ表示処理(showAnswerメソッド)
-//Todo 1. こたえの表示（画像・文字）
-//Todo 2. ボタン①を「次の問題にすすむ」に
-//Todo 3. 最後の問題まで来たら
-//=> Todo 1. テスト状態を「終了」にしてメッセージ表示
-//=> Todo 2. ボタン①を見えない＆使えないように
-//=> Todo 3. ボタン②を「もどる」に    }
+        // こたえ表示処理(showAnswerメソッド)
+        // 1. こたえの表示（画像・文字）
+        imageViewFlashAnswer.visibility = View.VISIBLE
+        textViewFlashAnswer.text = wordList[intCount - 1].strAnswer
+
+        // 2. ボタン①を「次の問題にすすむ」に
+        buttonNext.setBackgroundResource(R.drawable.image_button_go_next_question)
+
+        // 3. 最後の問題まで来たら
+        if (intLength == intCount) {
+            //=>  1. テスト状態を「終了」にしてメッセージ表示
+            intState = TEST_FINISHED
+            textViewMessage.text = "テスト修了"
+
+            //=>  2. ボタン①を見えない＆使えないように
+            buttonNext.isEnabled = false
+            buttonNext.visibility = View.INVISIBLE
+
+            //=>  3. ボタン②を「もどる」に
+            buttonEndTest.setBackgroundResource(R.drawable.image_button_back)
+
+        }
     }
 
-        private fun showQuestion() {
+    private fun showQuestion() {
+        // 問題表示処理(showQuestionメソッド)
 
-//Todo 問題表示処理(showQuestionメソッド)
-//Todo 1. 前の問題の暗記済フラグをDB登録（更新）
-//Todo 2. のこり問題数を１つ減らして表示
-//Todo 3. 今回の問題表示・前の問題消去（画像と文字）
-//Todo 4. ボタン①を「こたえを見る」ボタンに
-//Todo 5. 問題の単語が暗記済の場合はチェックを入れる}
 
+        // Todo 1. 前の問題の暗記済フラグをDB登録（更新）
+        if (intCount > 0) {  //２問目以降のときのみ発生
+            val selectedDB = realm.where(WordDB::class.java).equalTo(getString(R.string.db_field_question),
+                wordList[intCount - 1].strQuestion).findFirst()
+            realm.beginTransaction()
+            selectedDB?.boolMemoryFrag = boolMemorized
+            realm.commitTransaction()
         }
 
 
+        // 2. のこり問題数を１つ減らして表示
+        intCount++
+        textViewRemaining.text = (intLength - intCount).toString()
+
+
+        // 3. 今回の問題表示・前のこたえ消去（画像と文字）
+        imageViewFlashAnswer.visibility = View.INVISIBLE
+        textViewFlashAnswer.text = ""
+        imageViewFlashQuestion.visibility = View.VISIBLE
+        textViewFlashQuestion.text = wordList[intCount - 1].strQuestion
+
+        // 4. ボタン①を「こたえを見る」ボタンに
+        buttonNext.setBackgroundResource(R.drawable.image_button_go_answer)
+
+        // 5. 問題の単語が暗記済の場合はチェックを入れる
+        checkBox.isChecked = wordList[intCount - 1].boolMemoryFrag
+        boolMemorized = checkBox.isChecked
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
